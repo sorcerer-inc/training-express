0b956d1e25d503058758664c37f3776e07ed74a7
wym <wym@sorcerer.co.jp>
2023-04-14 18:50:44 +0900

    delete import

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,5 @@
 import * as express from "express";
 import { UserController } from "../controllers";
-import { dbPool } from "../helpers/db-helper";
 export const router = express.Router();
 
 const userController = new UserController();
