e43b379a4e5a097af7c7b2151cd32b328ac34722
wym <wym@sorcerer.co.jp>
2023-04-18 12:47:18 +0900

    コード修正

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -4,7 +4,7 @@ import { dbPool, transactionHelper } from "../helpers/db-helper";
 import { User } from "../interfaces/user";
 
 export class UserController {
-  async getAllUsers(req: Request, res: Response, next: NextFunction) {
+  async getAllUsers(req: Request, res: Response, next: NextFunction): Promise<void> {
     const dbConnection = await dbPool.getConnection();
     try {
       await dbConnection.beginTransaction(); // トランザクション例
@@ -21,13 +21,8 @@ export class UserController {
     }
   }
 
-  async createUser(req: Request, res: Response, next: NextFunction) {
-    if (
-      !req.body.name ||
-      !req.body.password ||
-      !req.body.money ||
-      !req.body.hp
-    ) {
+  async createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
+    if (!req.body.name || !req.body.password || !req.body.money || !req.body.hp) {
       res.status(400).json({ message: "Invalid parameters or body." });
       return;
     }
