60b6cebe81172a9d4925ba8c7f28bcecf37feb77
wym <wym@sorcerer.co.jp>
2023-04-26 11:23:12 +0900

    user-service.test修正

--- tests/user-service.test.ts
+++ tests/user-service.test.ts
@@ -2,44 +2,44 @@ import { getAllUsers, createUser } from "../src/services/user-service";
 import { User } from "../src/interfaces/user";
 import * as userModel from "../src/models/user-model";
 
-const mock_data_user_1: User = {
+const mockUser1: User = {
   id: 1,
   name: "user1",
   password: "password",
   money: 10,
   hp: 10,
 };
-const mock_data_user_2: User = {
+const mockUser2: User = {
   id: 2,
   name: "user2",
   password: "password",
   money: 10,
   hp: 10,
 };
-const mock_data_all_users: User[] = [mock_data_user_1, mock_data_user_2];
-const mock_data_all_users_empty: User[] = [];
+const mockUsers: User[] = [mockUser1, mockUser2];
+const mockUsersEmpty: User[] = [];
 
 test("get all users", async () => {
   jest
     .spyOn(userModel, "getAllUsers")
-    .mockResolvedValueOnce(mock_data_all_users)
-    .mockResolvedValueOnce(mock_data_all_users_empty);
+    .mockResolvedValueOnce(mockUsers)
+    .mockResolvedValueOnce(mockUsersEmpty);
 
   let conn: any;
-  expect(await getAllUsers(conn)).toEqual(mock_data_all_users);
-  expect(await getAllUsers(conn)).toEqual(mock_data_all_users_empty);
+  expect(await getAllUsers(conn)).toEqual(mockUsers);
+  expect(await getAllUsers(conn)).toEqual(mockUsersEmpty);
 });
 
 test("create user", async () => {
   jest.spyOn(userModel, "createUser").mockImplementation(async (data: User) => {
-    mock_data_all_users.push({
-      id: mock_data_all_users.length + 1,
+    mockUsers.push({
+      id: mockUsers.length + 1,
       name: data.name,
       password: data.password,
       money: data.money,
       hp: data.hp,
     });
-    return mock_data_all_users.length;
+    return mockUsers.length;
   });
 
   let conn: any;
@@ -48,5 +48,26 @@ test("create user", async () => {
       { name: "test", password: "test", money: 10, hp: 10 },
       conn
     )
-  ).toEqual(mock_data_all_users.length);
+  ).toEqual(mockUsers.length);
+});
+
+test("create user例2", async () => {
+  jest
+    .spyOn(userModel, "createUser")
+    .mockResolvedValueOnce(1)
+    .mockResolvedValueOnce(2);
+
+  let conn: any;
+  expect(
+    await createUser(
+      { name: "test1", password: "test", money: 10, hp: 10 },
+      conn
+    )
+  ).toEqual(1);
+  expect(
+    await createUser(
+      { name: "test2", password: "test", money: 10, hp: 10 },
+      conn
+    )
+  ).toEqual(2);
 });
